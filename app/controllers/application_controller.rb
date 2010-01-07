066f78dcd6009431df9de53e4a393c75a3e58377
tonkapark <matt@tonkapark.com>
2010-01-07 14:04:31 -0600

    start new plugin clearance-invite to handle authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,14 @@
 
 class ApplicationController < ActionController::Base  
   include Clearance::Authentication
-  
-  # FIXME: I don't think we want to include ALL helpers here, just possibly the application-helper
   helper :all # include all helpers, all the time
  
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'e09cdc0512f2d77a60d7ccb4c46775c1'
  
-  # Authlogic
   filter_parameter_logging  :password, :password_confirmation
-  helper_method :current_user_session, :current_user
- 
+   
  
   ## Declare exception to handler methods
   rescue_from ActiveRecord::RecordNotFound, :with => :bad_record
